b46993fcd020fb0166edb4a5aad726d90c24ca50
Don <donNewtonAlpha@gmail.com>
2018-07-30 13:33:20 -0400

    renaming main.go to Abstract.go

--- cmd/AbstractOLT.go
+++ cmd/AbstractOLT.go
@@ -12,8 +12,8 @@ func main() {
 	var slots [8]models.Slot
 	for i := 0; i < 8; i++ {
 		slot := models.Slot{Parent: &chassis}
+		fmt.Printf("slot %d\n", i)
 		slots[i] = slot
-		chassis.Slots = slots
 	}
-
+	chassis.Slots = slots
 }
