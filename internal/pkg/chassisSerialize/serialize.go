86c866964787e4f9d1fab204a5312ea5532841c7
cm <eulercool@gmail.com>
2018-08-06 11:23:30 -0400

    Fixed pointer issue that failed to initialize parent

--- internal/pkg/chassisSerialize/serialize.go
+++ internal/pkg/chassisSerialize/serialize.go
@@ -7,23 +7,27 @@ import (
 )
 
 
-func Serialize(chassis models.Chassis) ([]byte, error) {
+func Serialize(chassis *models.Chassis) ([]byte, error) {
 	return json.Marshal(chassis)
 }
 
-func Deserialize(jsonData []byte) (models.Chassis, error) {
+func Deserialize(jsonData []byte) (*models.Chassis, error) {
 	var chassis models.Chassis
 	err := json.Unmarshal(jsonData, &chassis)
 
-	for _, slot := range chassis.Slots {
+	for i := 0; i < len(chassis.Slots); i++ {
+		var slot *models.Slot
+		slot = &chassis.Slots[i]
 		slot.Parent = &chassis
-		for _, port := range slot.Ports {
-			port.Parent = &slot
-			for _, ont := range port.Onts {
-				ont.Parent = &port
+		for j := 0; j < len(slot.Ports); j++ {
+			var port *models.Port
+			port = &slot.Ports[j]
+			port.Parent = slot
+			for k := 0; k < len(port.Onts); k++ {
+				port.Onts[k].Parent = port
 			}
 		}
 	}
 
-	return chassis, err
+	return &chassis, err
 }
