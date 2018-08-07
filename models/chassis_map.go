2afc3b5c3d4be380d25ed6587ae4ccf9fa0965b8
donNewtonAlpha <donNewtonAlpha@gmail.com>
2018-08-07 11:18:16 -0400

    interim commit

--- models/chassis_map.go
+++ models/chassis_map.go
@@ -4,17 +4,32 @@ import (
 	"fmt"
 
 	"github.com/donNewtonAlpha/AbstractOLT/models/abstractOLTModel"
+	"github.com/donNewtonAlpha/AbstractOLT/models/physicalModel"
 )
 
 var chassisMap map[string]*abstractOLTModel.Chassis
+var abstractChassisMap map[string]*abstractOLTModel.Chassis
 
 /*
-GetChassisMap return the chassis map singleton
+GetPhyChassisMap return the chassis map singleton
 */
-func GetChassisMap() *map[string]*abstractOLTModel.Chassis {
+func GetPhyChassisMap() *map[string]*abstractOLTModel.Chassis {
 	if chassisMap == nil {
 		fmt.Println("chassisMap was nil")
-		chassisMap = make(map[string]*abstractOLTModel.Chassis)
+		chassisMap = make(map[string]*physicalModel.Chassis)
+
+	}
+	fmt.Printf("chassis map %v\n", chassisMap)
+	return &chassisMap
+}
+
+/*
+GetAbstractChassisMap return the chassis map singleton
+*/
+func GetAbstractChassisMap() *map[string]*abstractOLTModel.Chassis {
+	if abstractChassisMap == nil {
+		fmt.Println("chassisMap was nil")
+		abstractChassisMap = make(map[string]*abstractOLTModel.Chassis)
 
 	}
 	fmt.Printf("chassis map %v\n", chassisMap)
