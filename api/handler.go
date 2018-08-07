8c87e173f3e7a0679540d8336b50f69e407e3c01
donNewtonAlpha <donNewtonAlpha@gmail.com>
2018-08-07 15:43:48 -0400

    moving to single add chassis api call

--- api/handler.go
+++ api/handler.go
@@ -5,7 +5,6 @@ import (
 	// "net"
 
 	"github.com/donNewtonAlpha/AbstractOLT/models"
-	"github.com/donNewtonAlpha/AbstractOLT/models/abstractOLTModel"
 	"github.com/donNewtonAlpha/AbstractOLT/models/physicalModel"
 	context "golang.org/x/net/context"
 )
@@ -19,27 +18,15 @@ type Server struct {
 /*
 CreateChassis - allocates a new Chassis struct and stores it in chassisMap
 */
-func (s *Server) CreatePhyChassis(ctx context.Context, in *AddPhyChassisMessage) (*AddPhyChassisReturn, error) {
+func (s *Server) CreateChassis(ctx context.Context, in *AddChassisMessage) (*AddChassisReturn, error) {
 	chassisMap := models.GetPhyChassisMap()
 	clli := in.GetCLLI()
 	chassis := (*chassisMap)[clli]
 	if chassis != nil {
-		return &AddPhyChassisReturn{DeviceID: chassis.CLLI}, nil
+		return &AddChassisReturn{DeviceID: chassis.CLLI}, nil
 	}
 	newChassis := physicalModel.Chassis{CLLI: clli}
 	fmt.Printf("new chassis %v\n", newChassis)
 	(*chassisMap)[clli] = &newChassis
-	return &AddPhyChassisReturn{DeviceID: newChassis.CLLI}, nil
-}
-func (s *Server) CreateAbstractChassis(ctx context.Context, in *AddAbstractChassisMessage) (*AddAbstractChassisReturn, error) {
-	chassisMap := models.GetAbstractChassisMap()
-	clli := in.GetCLLI()
-	chassis := (*chassisMap)[clli]
-	if chassis != nil {
-		return &AddAbstractChassisReturn{DeviceID: chassis.CLLI}, nil
-	}
-	newChassis := abstractOLTModel.Chassis{CLLI: clli}
-	fmt.Printf("new chassis %v\n", newChassis)
-	(*chassisMap)[clli] = &newChassis
-	return &AddAbstractChassisReturn{DeviceID: newChassis.CLLI}, nil
+	return &AddChassisReturn{DeviceID: newChassis.CLLI}, nil
 }
