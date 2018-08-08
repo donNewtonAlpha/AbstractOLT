4db4f87401e7d700a583ad4eab394c19d4b1812d
donNewtonAlpha <donNewtonAlpha@gmail.com>
2018-08-08 12:13:27 -0400

    fixed spacing

--- models/abstractOLTModel/chassis.go
+++ models/abstractOLTModel/chassis.go
@@ -3,22 +3,18 @@ package abstractOLTModel
 const MAX_SLOTS int = 16
 const MAX_PORTS int = 16
 
-
 /*
 Chassis is a model that takes up to 16 discreet OLT chassis as if it is a 16 slot OLT chassis
 */
-
 type Chassis struct {
-	CLLI         string
-	Slots        [16]Slot
-	AllocInfo    PortAllocationInfo
+	CLLI      string
+	Slots     [16]Slot
+	AllocInfo PortAllocationInfo
 }
-
 
 type PortAllocationInfo struct {
 	// Current info on next port to be allocated
 	slot       int
-	port	   int
+	port       int
 	outOfPorts bool
 }
-
